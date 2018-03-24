cae45358e1755cf01af434c4fc19adda98c31193
eric.wtlin <eric.wtlin@gmail.com>
2018-03-24 16:08:08 +0800

    add logging

--- start_server.sh
+++ start_server.sh
@@ -15,9 +15,9 @@ fi
 
 if [ $# -eq 0 ]
 then
-    nohup python ./shadowsocksR/shadowsocks/server.py -c ./config.json >/dev/null 2>&1 &
+    nohup python ./shadowsocksR/shadowsocks/server.py -c ./config.json >/var/log/shadowsocksr.server.log 2>&1 &
 else
-    nohup python ./shadowsocksR/shadowsocks/server.py -c $1 >/dev/null 2>&1 &
+    nohup python ./shadowsocksR/shadowsocks/server.py -c $1 >/var/log/shadowsocksr.server.log 2>&1 &
 fi  
 
 count=`ps -ef |grep "shadowsocks/server.py" |grep -v "grep" |wc -l`
